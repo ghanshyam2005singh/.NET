17909f74920daa15d5b3708e27d4ac7c5631c2f7
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-08-06 22:15:41 +0530

    first-commit

---
+++ hello.cs
@@ -0,0 +1,8 @@
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("Welcome to C# programming.");
+    }
+}
